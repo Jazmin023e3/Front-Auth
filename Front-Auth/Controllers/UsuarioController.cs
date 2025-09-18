a008b045aa202a4fd144dc6bf7c85fc579d81f09
Jazmin Aguilar <Jazmin086441@outlook.es>
2025-09-18 15:30:35 -0600

    se avanzo

---
+++ Front-Auth/Controllers/UsuarioController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FrontAuth.WebApp.Services;
+using System.Threading.Tasks;
+using FrontAuth.WebApp.DTOs.UsuarioDTOs;
+using FrontAuth.WebApp.Helpers;
+
+namespace FrontAuth.WebApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly ApiService _apiService;
+
+        public UsuarioController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        // Listar usuarios
+        public async Task<IActionResult> Index()
+        {
+            var token = AuthHelper.ObtenerToken(User); // Obtener token desde Claims
+            var usuarios = await _apiService.GetAllAsync<UsuarioDTO>("user/usuarios", token);
+            return View(usuarios);
+        }
+    }
+}
